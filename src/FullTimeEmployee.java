83cec141334fe434f4c5e2e7fc2db078c09af174
raipramod <pcrai58@gmail.com>
2024-07-16 15:53:59 -0600

    Add FullTime Employee

---
+++ src/FullTimeEmployee.java
@@ -0,0 +1,20 @@
+// Created FullTime Employee class and declared attributes
+class FullTimeEmployee extends EmployeeApp {
+
+    private double monthlySalary;
+
+    // Created the constructor of FullTime Employee
+    public FullTimeEmployee(String name, int id, double monthlySalary){
+        // Used super keyword to get the parent class attribute in child class
+        super(name, id);
+
+        this.monthlySalary = monthlySalary;
+    }
+
+    // Implemented calculate salary abstract method
+    @Override
+    public double calculateSalary(){
+        return calculateSalary();
+    }
+
+}
