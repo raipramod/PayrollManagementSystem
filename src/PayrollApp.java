c8e467624f5f6faa76420939716d6de3bbc3c7f9
raipramod <pcrai58@gmail.com>
2024-07-16 15:16:23 -0600

    Payroll Parent class added

---
+++ src/PayrollApp.java
@@ -0,0 +1,10 @@
+/** Author : Pramod Rai
+ * Description : Implemented all OOPs concept to develop Employee Management System in Java
+ * Date : July 14 2024
+ */
+public class PayrollApp {
+    public static void main(String[] args) {
+
+        }
+    }
+}
