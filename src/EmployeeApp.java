d088e243bce29995967263b55281b70fbf39584e
raipramod <pcrai58@gmail.com>
2024-07-16 15:37:45 -0600

    Added EmployeeApp Super class

---
+++ src/EmployeeApp.java
@@ -0,0 +1,30 @@
+    // Created EmployeeApp class and declared the attributes
+abstract class EmployeeApp {
+    private String name;
+    private int id;
+
+    // Created Constructor of the EmployeeApp class
+    public EmployeeApp(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+    // Implemented the encapsulation to get the name and id respectively
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    // Created abstract method for calculate the salary, abstract method has no method body
+    public abstract double calculateSalary();
+
+    // Override toString method and return all the attributes
+       @Override
+        public String toString(){
+        return "Employee [ name = "+name+", id = "+id+", salary = "+calculateSalary()+"]";
+    }
+
+
+}
