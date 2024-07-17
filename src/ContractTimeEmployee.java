c2f555a20f4bb369e041189965f3a187956636ce
raipramod <87155039+raipramod@users.noreply.github.com>
2024-07-17 13:20:41 -0600

    Update ContractTimeEmployee.java

--- src/ContractTimeEmployee.java
+++ src/ContractTimeEmployee.java
@@ -1,2 +1,17 @@
-public class ContractTimeEmployee {
+class ContractTimeEmployee extends EmployeeApp {
+    private boolean employeeBenefit;
+    private int fixedHour;
+    private double rate;
+
+    public ContractTimeEmployee(String name, int id, boolean employeeBenefit, int fixedHour, double rate){
+        super(name, id);
+        this.employeeBenefit = employeeBenefit;
+        this.rate = rate;
+        this.fixedHour = fixedHour;
+    }
+
+    @Override
+    public double calculateSalary(){
+       return fixedHour * rate;
+    }
 }
