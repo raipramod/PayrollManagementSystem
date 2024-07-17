cfefe49878731ab1a97bdb8833917ea553a8ec22
raipramod <pcrai58@gmail.com>
2024-07-17 15:13:35 -0600

    Add Payroll App file

--- src/PayrollApp.java
+++ src/PayrollApp.java
@@ -4,6 +4,21 @@
  */
 public class PayrollApp {
     public static void main(String[] args) {
+        PayrollSystemApp payrollSystemApp = new PayrollSystemApp();
+        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee("Rai", 32, 70000);
+        PartTimeEmployee partTimeEmployee = new PartTimeEmployee("Jon", 19, 20, 15);
+        ContractTimeEmployee contractTimeEmployee = new ContractTimeEmployee("Purna", 23,Boolean.parseBoolean("FALSE"), 10, 12);
+
+        System.out.println("Initial Employee Info");
+//        payrollSystemApp.addEmployee(fullTimeEmployee);
+//        payrollSystemApp.addEmployee(partTimeEmployee);
+//        payrollSystemApp.addEmployee(contractTimeEmployee);
+//        payrollSystemApp.displayEmployees();
+
+//        System.out.println("After Removing the some Info");
+//        payrollSystemApp.removeEmployee(32);
+//        payrollSystemApp.displayEmployees();
+
         }
     }
 
